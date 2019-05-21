2673c6404c6f02491a3588e08763e601bd714386
heftyfunseeker <niccosimone@gmail.com>
2019-05-20 22:03:16 -0700

    executing instructions. locked at 1 hz for debugging/developing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use hack_assembler;
 use std::fs;
+use std::thread;
 
 const ADDRESSABLE_MEM_SIZE: usize = 0x7FFF;
 
 struct Memory32K {
-    memory : Vec<u16>,
+    memory : Vec<i16>,
 }
 
 impl Memory32K {
@@ -17,13 +18,13 @@ impl Memory32K {
 
 struct CPU {
     /// A register
-    a : u16,
+    a : i16,
     
     /// D register
-    d : u16,
+    d : i16,
     
     /// Program counter
-    pc: u16,
+    pc: i16,
 }
 
 impl CPU {
@@ -43,7 +44,6 @@ struct HackEmulator {
     cpu : CPU,
 }
 
-// @note: single cycle fetch execute because of partitioned instruction memory
 impl HackEmulator {
     fn new() -> HackEmulator {
         return HackEmulator { 
@@ -55,7 +55,7 @@ impl HackEmulator {
     }
     
     /// helper to obtain the 'm register'
-    fn m<'a>(&'a mut self) -> &'a mut u16 {
+    fn m<'a>(&'a mut self) -> &'a mut i16 {
         return &mut self.ram.memory[self.cpu.a as usize];
     }
     
@@ -72,47 +72,73 @@ impl HackEmulator {
         hack_assembler::generate_binary(assembler_output, &mut binary);
         
         // copy the instructions into rom
-        // @todo: do we need to track the loaded instruction count? probably should
         self.num_instructions = binary.len();
         for i in 0..self.num_instructions {
-            self.rom.memory[i] = binary[i];
+            self.rom.memory[i] = binary[i] as i16;
         }
     }
     
-    fn run(&mut self, hertz: usize) {
+    fn run(&mut self) {
         loop {
-            
+            print!("A: {}\nD: {}\nPC: {}\n\n", self.cpu.a, self.cpu.d, self.cpu.pc);
+            self.execute(1);
+            thread::sleep(std::time::Duration::from_secs(1));
         }
-        
     }
     
     /// decodes and executes instructions for duration_in_cycles. Note: 
     /// a fetch/decode/execute is only 1 clock cycle because the rom is partitioned
-    fn execute(&mut self, duration_in_cycles: usize) {
-        let instruction = self.rom.memory[self.cpu.pc as usize];
-        
-        // a-instruction
-        if instruction & 0x8000 == 0 {
-            self.cpu.a = instruction & 0x7FFF;
-        } 
-        else {
+    fn execute(&mut self, duration_in_cycles : usize) {
+        for _ in 0..duration_in_cycles {
+            let instruction = self.rom.memory[self.cpu.pc as usize];
             
+            // a-instruction
+            let is_jump;
+            if instruction as u16 & 0x8000 == 0 {
+                self.cpu.a = instruction & 0x7FFF;
+                is_jump = false;
+            } 
+            else {
+                is_jump = self.execute_c_instruction(instruction);
+            }
+            
+            if is_jump {
+                self.cpu.pc = self.cpu.a;
+            }
+            else {
+                self.cpu.pc += 1;
+            }
         }
-        
     }
     
-    fn execute_c_instruction(&mut self, instruction: u16) {
-        let comp_raw = instruction & 0xFC0;
-        let dest_raw = instruction & 0x38;
+    fn execute_c_instruction(&mut self, instruction: i16) -> bool {
+        let comp_raw = (instruction & 0xFC0) >> 6 ;
+        let dest_raw = (instruction & 0x38) >> 3;
         let jump_raw = instruction & 0x7;
         let a_raw = instruction & 0x1000;
         
-        let comp = 1; 
-        // match comp_raw {
-        //     0b101010 {
-        // 
-        //     }
-        // };
+        let comp; 
+        match comp_raw {
+            0b101010 => comp = 0,
+            0b111111 => comp = 1,
+            0b111010 => comp = -1,    
+            0b001100 => comp = self.cpu.d,
+            0b110000 => comp = if a_raw != 0 { *self.m() } else { self.cpu.a },
+            0b001101 => comp = !self.cpu.d,
+            0b110001 => comp = if a_raw != 0 { !*self.m() } else { !self.cpu.a },
+            0b001111 => comp = -self.cpu.d,
+            0b110011 => comp = if a_raw != 0 { -*self.m() } else { -self.cpu.a },
+            0b011111 => comp = self.cpu.d + 1,
+            0b110111 => comp = if a_raw != 0 { *self.m() + 1 } else { self.cpu.a + 1 },
+            0b001110 => comp = self.cpu.d - 1,
+            0b110010 => comp = if a_raw != 0 { *self.m() - 1 } else { self.cpu.a - 1 },
+            0b000010 => comp = if a_raw != 0 { *self.m() + self.cpu.d } else { self.cpu.a + self.cpu.d },
+            0b010011 => comp = if a_raw != 0 { self.cpu.d - *self.m() } else { self.cpu.d  - self.cpu.a },
+            0b000111 => comp = if a_raw != 0 { *self.m() - self.cpu.d } else { self.cpu.a - self.cpu.d },
+            0b000000 => comp = if a_raw != 0 { *self.m() & self.cpu.d } else { self.cpu.a & self.cpu.d },
+            0b010101 => comp = if a_raw != 0 { *self.m() | self.cpu.d } else { self.cpu.a | self.cpu.d },
+            _ => { panic!("could not decode compute part of c instruction") }
+        }
         
         if dest_raw & 0b001 != 0 {
             *self.m() = comp;
@@ -123,12 +149,28 @@ impl HackEmulator {
         if dest_raw & 0b100 != 0 {
             self.cpu.a = comp;
         }
+        
+        let is_jump;
+        match jump_raw {
+            0b000 => is_jump = false,
+            0b001 => is_jump = comp > 0,
+            0b010 => is_jump = comp == 0,
+            0b011 => is_jump = comp >= 0,
+            0b100 => is_jump = comp < 0,
+            0b101 => is_jump = comp != 0,
+            0b110 => is_jump = comp <= 0,
+            0b111 => is_jump = true,
+            _ => { panic!("can't decode jump part of c instruction") }
+        }
+        
+        return is_jump;
     }
 }
 
 fn main() {
     let mut emulator = HackEmulator::new();
     emulator.load_rom_from_file("test.asm");
+    emulator.run();
     
     // debug dump instructions in rom
     for i in 0..emulator.num_instructions {
